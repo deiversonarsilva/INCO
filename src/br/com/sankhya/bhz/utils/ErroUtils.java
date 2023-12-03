cd81927ee7e2cf4e60c2793f18bfc28cca3fa3ee
deiverson <deiverson.silva@bhzsolucoes.com.br>
2023-12-03 02:50:39 -0300

    commit inicial

---
+++ src/br/com/sankhya/bhz/utils/ErroUtils.java
@@ -0,0 +1,29 @@
+package br.com.sankhya.bhz.utils;
+
+public class ErroUtils {
+
+    public static void disparaErro(String msg) throws Exception {
+        String msgTratada = "<hr>" +
+                "<b><span style=\"font-size: 1.2em\">" + msg + "</span></b><hr>";
+        throw new Exception(msgTratada);
+    }
+    public static void ErroFormatado(String msg, String motivo, String solucao) throws Exception {
+        String m = null;
+        String s= null;
+        if(motivo != null){
+            m = "<b>Motivo: </b> "+motivo+ "<br><br>";
+        }else {
+            m = "<br><br>";
+        }
+        if(solucao != null){
+            s = "<b>Solução: </b> "+solucao+ "</span></b><hr>";
+        }else {
+            s = "</span></b><hr>";
+        }
+        String msgTratada = "<hr>" +
+                "<p align=\"left\"><font size=\"3\" face=\"arial\" color=\"#8B1A1A\"><b>Atenção:  </b>" + msg +"<b>  --Ação Cancelada--</b>  <br><br>"
+                +m
+                +s;
+        throw new Exception(msgTratada);
+    }
+}
---
+++ src/br/com/sankhya/bhz/utils/acaoExecutaSQL.java
@@ -0,0 +1,18 @@
+package br.com.sankhya.bhz.utils;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+import br.com.sankhya.jape.dao.JdbcWrapper;
+import br.com.sankhya.jape.sql.NativeSql;
+import br.com.sankhya.modelcore.util.EntityFacadeFactory;
+
+public class acaoExecutaSQL implements AcaoRotinaJava {
+    @Override
+    public void doAction(ContextoAcao contexto) throws Exception {
+        JdbcWrapper jdbc = EntityFacadeFactory.getDWFFacade().getJdbcWrapper();
+        jdbc.openSession();
+        NativeSql sql = new NativeSql(jdbc);
+        sql.loadSql(acaoExecutaSQL.class, "sql/comando.sql");
+        sql.executeUpdate();
+    }
+}
